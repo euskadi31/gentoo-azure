1ac9241bce09d328b296b916cd6b4c916094b730
Axel Etcheverry <euskadi31@gmail.com>
2014-09-29 16:27:34 +0200

    chore(portage): add vim and eix package

--- scripts/portage.sh
+++ scripts/portage.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+cp -f $SCRIPTS/scripts/make.conf /mnt/gentoo/etc/portage/
+
 chroot /mnt/gentoo /bin/bash <<'EOF'
 mkdir /usr/portage
 emerge-webrsync
+emerge app-portage/eix app-editors/vim app-portage/layman
 EOF
